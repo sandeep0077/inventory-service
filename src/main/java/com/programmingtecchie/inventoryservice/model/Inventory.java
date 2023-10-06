d539ca45747c583271c804928e4f49258dd6a6f3
Sandeep <sm671903@gmail.com>
2023-10-06 15:35:41 +0530

    Initial commit

---
+++ src/main/java/com/programmingtecchie/inventoryservice/model/Inventory.java
@@ -0,0 +1,22 @@
+package com.programmingtecchie.inventoryservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "t_inventory")
+public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String skuCode;
+    private Integer quantity;
+
+}
